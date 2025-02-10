b54041bdd211c050aa9e7ceacb9b85353553b2f4
Mohammed Elghanam <144786058+MohammedElghanam@users.noreply.github.com>
2025-02-10 16:57:57 +0100

    feat: make component of login. MEW-11 #done

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -11,28 +11,33 @@ const Login = () => {
     <View style={styles.container}>
         <Image
             source={require('../../assets/images/imageLogin.jpg')}
-            style={{ width: 200, height: 200, alignSelf: 'center' , marginBottom: 1}}
+            style={{ width: 200, height: 200, alignSelf: 'center' , marginBottom: 10, borderRadius: 500}}
         />        
 
-        <View style={[ styles.inputContainer, errors.password ? { borderColor: 'red' } : null ]}>
+        <View style={
+            styles.inputContainer
+            // [ styles.inputContainer, errors.password ? { borderColor: 'red' } : null ]
+        }>
             <Ionicons 
                 name="lock-closed-outline" 
                 size={20} 
-                color={ errors.password ? 'red' : "gray" } 
+                // color={ errors.password ? 'red' : "gray" } 
             />
             <TextInput
                 style={styles.input}
                 placeholder="Password"
-                placeholderTextColor={errors.password ? 'red' : 'gray'}
+                // placeholderTextColor={errors.password ? 'red' : 'gray'}
                 secureTextEntry
-                value={password}
-                onChangeText={setPassword}
+                // value={password}
+                // onChangeText={setPassword}
             />
         </View>
-        {errors.password && <Text style={styles.error}>{errors.password}</Text>}
+        {/* {errors.password && <Text style={styles.error}>{errors.password}</Text>} */}
 
         <View style={styles.btn}>
-            <Text style={styles.text} onPress={handleSubmit} >Login</Text>
+            <Text style={styles.text} 
+                // onPress={handleSubmit} 
+            >Login</Text>
         </View>
 
     </View>
@@ -46,12 +51,6 @@ const styles = StyleSheet.create({
     alignContent: 'center',
     padding: 20,
     backgroundColor: '#fff',
-  },
-  header: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-    textAlign: 'center',
   },
   inputContainer: {
     flexDirection: 'row',
@@ -70,7 +69,7 @@ const styles = StyleSheet.create({
   },
   btn: {
     height: 50,
-    backgroundColor: '#6d1fad',
+    backgroundColor: '#B2A5FF',
     padding: 10,
     borderRadius: 5,
     justifyContent: 'center',
@@ -85,9 +84,6 @@ const styles = StyleSheet.create({
   link: {
     color: '#3b82f6', 
     textDecorationLine: 'underline',
-  },
-  user: {
-    color: '#333',
   },
   error: {
     paddingHorizontal: 10,
