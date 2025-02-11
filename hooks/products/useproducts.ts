5bd0ea3af1bc15757290889ddb8fb811e9338a6d
Mohammed Elghanam <144786058+MohammedElghanam@users.noreply.github.com>
2025-02-11 17:17:27 +0100

    feat: cleaning the code of display list user by add hook

--- hooks/products/useproducts.ts
+++ hooks/products/useproducts.ts
@@ -1,5 +1,28 @@
-export default function useproducts() {
+import { useEffect, useState } from 'react';
 
+export default function useProducts() {
 
-  return 
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+            const response = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/products`);
+            const data = await response.json();
+            setProducts(data);
+            } catch (error) {
+            console.error('Error fetching products:', error);
+            } finally {
+            setLoading(false);
+            }
+        };
+        
+        fetchProducts();
+    }, []);
+
+  return {
+    products,
+    loading,
+  }
 }
